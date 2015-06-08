a583d1114e576b63aa4f09418f47647c221da2e4
chirath <chirath.02@gmail.com>
2015-06-08 22:23:58 +0530

    Added a little bit of css

--- products.php
+++ products.php
@@ -3,9 +3,11 @@ echo "<!DOCTYPE html>
                 <html>
                         <head>
                                 <title>Products</title>
+                                <link rel='stylesheet' href='css/bootstrap.css'>
+                                <link rel='stylesheet' href='css/main.css'>
                         </head>
                         <body>
-                                <h1>Welcome</h1>"
+                                <h1>Welcome</h1>";
 ?>
 <?php
 $servername = "localhost";
@@ -22,15 +24,28 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM products";
 $result = $conn->query($sql);
-
+echo "<div class='produsts'>
+              <div class='container'>
+              <div class='row'>";
 if ($result->num_rows > 0) {
     echo "<table><tr><th>ID</th><th>Name</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>".$row["name"]."</td><td>".$row["discription"]." ".$row["price"]."</td></tr>";
-        echo "<img src='images/" . $row["image"] . "'>";
+        
+        echo "
+	            <div class='col-md-4'>
+	                <div class='thumbnail'>
+	                    <img src='images/" . $row["image"] . "'>
+	                </div>
+                    </div>
+              
+        ";
         
     }
+    echo "</div>
+              </div>
+              </div>";
     echo "</table>";
 } else {
     echo "0 results";
